34d6f4905258ba2575ccfdd6b38d1d7dd3b33fa2
JoelhansenMN <joelhansen22@gmail.com>
2024-05-30 10:10:32 -0500

    work in project, resume, and portfolio

--- src/components/nav/Nav1.jsx
+++ src/components/nav/Nav1.jsx
@@ -7,9 +7,9 @@ export default function Nav1() {
 
   return (
     <>
-      <h1>Joel Hansen</h1>
+      <h1 className='joel'>Joel Hansen</h1>
       <nav className="navigation">
-        <a href="">About Me</a>
+        <a href=''>About Me</a>
         <a href="#portfolio">Portfolio</a>
         <a href="#pricing">Resume</a>
         <a href="#pricing">Contact</a>
---
+++ src/components/pages/resume/Resume.jsx
@@ -0,0 +1,26 @@
+import './Resume.css';
+
+export default function Resume() {
+
+  return (
+
+    <>
+      <section>
+        <h2>Eduction</h2>
+          <p>University of Minnesota</p>
+          <p> University</p>
+          <p></p>
+
+      </section>
+      <aside>
+
+
+
+      </aside>
+
+
+
+
+    </>
+  )
+}
